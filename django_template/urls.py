562101a3db6656a81f0c86398a56eb539f48a5c1
JsanzanaJ <130805847+JsanzanaJ@users.noreply.github.com>
2023-06-27 14:42:03 -0400

    Update urls.py

--- django_template/urls.py
+++ django_template/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import mi_vista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns = [
     path('mi-vista/', mi_vista, name='mi-vista'),
 ]
